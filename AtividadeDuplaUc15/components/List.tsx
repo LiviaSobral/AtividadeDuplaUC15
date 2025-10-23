3eaac6eaaeb5bc4c8b5c23bf60c36be5aae0ca96
LIVIA HELOISA ALVES SOBRAL <03758569273@senacrs.edu.br>
2025-10-23 17:20:03 -0300

    AAA

---
+++ AtividadeDuplaUc15/components/List.tsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function List() {
+
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+});
