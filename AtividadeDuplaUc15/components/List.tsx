69b61a8ff2ccc8d268db3ea9f8f98f52ca548c2a
LIVIA HELOISA ALVES SOBRAL <03758569273@senacrs.edu.br>
2025-10-27 14:23:51 -0300

    feito o useEffect e useState

--- AtividadeDuplaUc15/components/List.tsx
+++ AtividadeDuplaUc15/components/List.tsx
@@ -3,6 +3,28 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 
 export default function List() {
 
+  const [active, setActive] = useState<boolean>(true)
+
+  async function funcao(){
+    try{
+        console.log("PLaceHolder Incrivel")
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos?completed=false')
+        const info = await response.json()
+        console.log(info)
+      }catch(e){
+        console.log("Erro ao carregar informações ", e)
+      }
+  } 
+  useEffect(() => {
+    funcao() //carrega imediatamente (sem ter q esperar 10 segundos) mas so 1 vez quando iniciar.
+
+    const interval = setInterval( async () => {
+      funcao()
+    }, 10000)
+    
+    return () => clearInterval(interval);
+    },[active])
+
   return (
     <View style={styles.container}>
       
